2ffb9a82dd08f205208e1af279319b211abc1c78
Luiz Alfonso Glasenapp <luizglasenapp@gmail.com>
2019-10-13 22:14:11 -0300

    Implementação do UC01

--- index.php
+++ index.php
@@ -46,7 +46,7 @@
 						<li><a href="aluno_ocorrencias.php">Ocorrências</a></li>
 					</ul></li>
 				<li class="nav-item" data-toggle="tooltip" data-placement="right"
-					title="Charts"><a class="nav-link" href="charts.html"> <i
+					title="Charts"><a class="nav-link" href="avisos.html"> <i
 						class="fa fa-fw fa-area-chart"></i> <span class="nav-link-text">Avisos</span>
 				</a></li>
 				<li class="nav-item" data-toggle="tooltip" data-placement="right"
